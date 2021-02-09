fa11002fa822322fda9bc42f03a20f1bdd25c525
Linus <linusstrangstad@gmail.com>
2021-02-09 23:46:43 +0100

    bug

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          React
-        </a>
+        ></a>
       </header>
     </div>
   );
